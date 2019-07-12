a487356c70338f934d48f1ae1e314f19254f3f50
fcanosa22 <52534713+fcanosa22@users.noreply.github.com>
2019-07-12 12:15:33 -0300

    Instruccion switch realizados del 1 al 6

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,30 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+//tomo la edad
+var mes = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch(mes){
+  case "Julio":
+  case "Agosto":
+
+  alert("Abrigate que hace frio!");
+  break;
+
+  case "Enero":
+  case "Febrero":
+  case "Marzo":
+  case "Abril":
+  case "Mayo":
+  case "Junio":
+  alert("Falta para el invierno!");
+  break;
+
+  default:
+  alert("Ya pasamos el frio!");
+  break;
+
+
+}
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,11 +1,37 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+//tomo la edad
+var mes = document.getElementById('mes').value;
+
+switch(mes){
+  case "Febrero":
+  alert("Este mes tiene 28 dias");
+  break;
+
+  case "Enero":
+  case "Marzo":
+  case "Mayo":
+  case "Julio":
+  case "Agosto":
+  case "Octubre":
+  case "Diciembre":
+  alert("Este mes tiene 31 dias");
+  break;
+  default:
+  alert("Este mes tiene 30 dias");
+  break;
+
+
+
+
+
+
+
+}
 
 //alert (mesDelAño);
-	
-	
+
+
 
 
 
