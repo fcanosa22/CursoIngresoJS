421991eae4e67cbb83f03b93aa0e64f34f243b22
fcanosa22 <52534713+fcanosa22@users.noreply.github.com>
2019-08-01 12:14:52 -0300

    Algunos hechos (Falta terminar)

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,19 @@
 function mostrar()
 {
+  var num1;
+  var num2;
 
+  num1 = prompt("Ingrese el primer numero");
+  num1 = parseInt(num1);
+
+  num2 = prompt("Ingrese el segundo numero");
+  num2 = parseInt(num2);
+
+  if(num1==num2){
+    alert("Tus numeros son iguales y son: " +num1+ " y " +num2);
+  }
+
+  if(num1<num2){
+
+  }
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,7 +1,19 @@
 
 function mostrar()
 {
+  var largo;
+  var ancho;
+  var perimetro;
 
+  largo = prompt("Ingrese el largo");
+  largo = parseInt(largo);
+
+  ancho = prompt("Ingrese el ancho");
+  ancho = parseInt(ancho);
+
+  perimetro = largo*2 + ancho*2
+
+  alert("El perimetro es: " +perimetro);
 
 
 
