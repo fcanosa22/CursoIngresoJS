eaf6d94c5ef7b85d2c448163b6a6cb4a467f3f3e
fcanosa22 <52534713+fcanosa22@users.noreply.github.com>
2019-08-01 20:15:53 -0300

    Hechos 1 al 6 (faltan terminar)

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -2,6 +2,9 @@ function mostrar()
 {
   var num1;
   var num2;
+  var suma;
+  
+
 
   num1 = prompt("Ingrese el primer numero");
   num1 = parseInt(num1);
@@ -10,10 +13,28 @@ function mostrar()
   num2 = parseInt(num2);
 
   if(num1==num2){
-    alert("Tus numeros son iguales y son: " +num1+ " y " +num2);
+    alert("Tus numeros son iguales: " +num1+ " y " +num2)
+
+  }else if(num1>num2){
+    alert("La resta es: " +(num1-num2));
+
+  }else{
+    alert("La suma es: "+(num1+num2));
+
+  }
+  suma = num1+num2;
+  
+
+  if(suma>10){
+    alert("La suma es: " +suma+ " y supero los 10");
+    
+    
   }
 
-  if(num1<num2){
 
-  }
+  
+
+ 
+
+  
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
+    var notas;
+    var contNotas = 0;
 
+    notas = prompt("Ingrese notas");
+    contNotas++
+
+    while(notas>=0 || notas<=10){
+        alert(notas);
+        break;
+    }
+
+    
+
+
+     }
+
+
+
+
+    
 }
